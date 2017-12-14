0f18b21c8b57848902cb67e69a0ab4a17d8b7603
MicCabeza22 <cabeza.michael@yahoo.com>
2017-12-14 14:51:05 -0600

    Added Problem 2 solution.

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
 public class Person {
+    protected String firstName;
+    protected String lastName;
+
+    public static void main(String[] args) {
+        try {
+            Person person = new Person("John", "Smith");
+
+            System.out.printf("%s %s", person.getFirstName(), person.getLastName());
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Person(String firstName, String lastName) throws IllegalArgumentException {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        if (firstName.equals("") || lastName.equals("")) {
+            throw new IllegalArgumentException("ERROR: NULL value found");
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
